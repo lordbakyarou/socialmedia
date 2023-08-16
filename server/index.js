54505796d31d03f8d670be59684e78c4c3150dab
Mayur Hanwate <frostblizard111@gmail.com>
2023-08-16 22:15:24 +0530

    register and login and userpath created

--- server/index.js
+++ server/index.js
@@ -8,6 +8,9 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import { register } from "./controllers/auth.js";
 
 // CONFIGURATIONS
 
@@ -34,6 +37,13 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
+
+//Route with files
+app.post("/auth/register", upload.single("picture"), register);
+
+//Routes
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 //Mongoose setup
 const PORT = process.env.PORT || 6001;
